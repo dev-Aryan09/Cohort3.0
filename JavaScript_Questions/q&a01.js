b8c0e659773dca8097080fe48d891df0e5fcc937
Aryan Saini <09aryan.lucky@gmail.com>
2026-05-25 11:45:32 +0530

    add JS variable practice questions

--- JavaScript_Questions/q&a01.js
+++ JavaScript_Questions/q&a01.js
@@ -13,11 +13,11 @@ console.table([1, 2, 3, 4, 5]);
 
 // ----------------------------------------
 
-var studentName = "Aryan";
-console.log(studentName);
+// var studentName = "Aryan";
+// console.log(studentName);
 
-var age = 22;
-console.log(age);
+// var age = 22;
+// console.log(age);
 
 /*
 // swapping two variables using a temporary variable
@@ -37,6 +37,7 @@ console.log(b);
 [a,b] = [b,a]
 */
 
+/*
 const PI = 3.14;
 console.log(PI);
 
@@ -52,3 +53,34 @@ var firstName = "Aryan";
 var lastName = "Saini";
 var fullName = firstName+" "+lastName
 console.log(fullName)
+*/
+
+/*
+var str = "sheryians";
+
+var num = 100;
+
+var bol = true; // or false
+
+var intentionalEmpty = null;
+
+var onlyDeclareation; // gives undefined
+
+console.log(
+  typeof str,
+  typeof num,
+  typeof bol,
+  typeof intentionalEmpty,
+  typeof onlyDeclareation,
+);
+
+var mobileNumber = 9784109875;
+console.log(mobileNumber);
+
+var noValue = null;
+console.log(noValue);
+
+var bigNum = 123456789000000000111111111000n;
+console.log(bigNum);
+*/
+
