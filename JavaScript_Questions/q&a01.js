48d912b5da5d3e4fd7cb501ad9e40e95e68c423d
Aryan Saini <09aryan.lucky@gmail.com>
2026-05-25 11:15:25 +0530

    add JS question practice

---
+++ JavaScript_Questions/q&a01.js
@@ -0,0 +1,54 @@
+/*
+console.log("Hello JavaScript");
+
+var name = "Aryan Saini";
+var age = 22;
+var city = "Sikar";
+console.log("My name is,", name, "My age is,", age, "and I am from", city, ".");
+
+console.warn("Beware coder!");
+
+console.table([1, 2, 3, 4, 5]);
+*/
+
+// ----------------------------------------
+
+var studentName = "Aryan";
+console.log(studentName);
+
+var age = 22;
+console.log(age);
+
+/*
+// swapping two variables using a temporary variable
+var a = 1;
+var b = 2;
+var temp;
+
+temp = b;
+
+b = a;
+a = temp;
+
+console.log(a);
+console.log(b);
+
+// using array destructuring
+[a,b] = [b,a]
+*/
+
+const PI = 3.14;
+console.log(PI);
+
+var dec;
+console.log(dec);
+
+var score=0;
+
+score += 10;
+console.log(score)
+
+var firstName = "Aryan";
+var lastName = "Saini";
+var fullName = firstName+" "+lastName
+console.log(fullName)
