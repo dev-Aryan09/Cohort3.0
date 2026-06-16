2e40c1b6b2a6267949294200e2ade21f99a2d34c
Aryan Saini <09aryan.lucky@gmail.com>
2026-06-16 15:56:16 +0530

    add todo list: delete via approach 1

--- DOM/todoList/script.js
+++ DOM/todoList/script.js
@@ -3,6 +3,23 @@ const addBtn = document.querySelector("#add");
 const todoList = document.querySelector(".todo-list");
 const editBtn = document.querySelector(".edit");
 
+const deleteBtn = () => {
+  /*
+Downside of this approach:  
+Every time you add a new todo, you’re re‑attaching listeners to all delete buttons (including old ones).
+That means if you add 5 todos, the first delete button might have 5 listeners stacked on it. It still works,
+but it’s inefficient and can cause duplicate actions.
+
+"Event Delegation" is the best approach for doing this.
+*/
+  const delBtns = document.querySelectorAll(".del");
+  delBtns.forEach((delBtn) => {
+    delBtn.addEventListener("click", (e) => {
+      e.target.closest(".li").remove();
+    });
+  });
+};
+
 addBtn.addEventListener("click", () => {
   if (input.value.trim() === "") return;
   todoList.innerHTML += ` <div class="li">
@@ -13,4 +30,5 @@ addBtn.addEventListener("click", () => {
             </div>
         </div>`;
   input.value = "";
+  deleteBtn();
 });
