b639792347f1205e2d3ba704c3f0351a0a5134a0
Aryan Saini <09aryan.lucky@gmail.com>
2026-07-25 16:40:13 +0530

    day14: routing using react-router

--- React/day14-react_router/src/App.jsx
+++ React/day14-react_router/src/App.jsx
@@ -2,30 +2,29 @@ import React, { useState } from "react";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
+import { Route, Routes, NavLink } from "react-router";
 
 const App = () => {
-  const [toggle, setToggle] = useState("home");
   return (
     <div className="h-screen p-2">
       <nav className="flex justify-between border p-2">
         <h1>Logo</h1>
         <ul className="flex gap-4 cursor-pointer">
-          <li onClick={() => setToggle("home")}>Home</li>
-          <li onClick={() => setToggle("about")}>About</li>
-          <li onClick={() => setToggle("contact")}>Contact</li>
-          <li>Services</li>
-          <li>Enterprise</li>
-          <li>Shopping</li>
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/about"}>About</NavLink>
+          <NavLink to={"/contact"}>Contact</NavLink>
         </ul>
         <button>Login</button>
       </nav>
 
-      {toggle === "home" && <Home />}
-      {toggle === "about" && <About />}
-      {toggle === "contact" && <Contact />}
-      {/* if we have 10 or 15 or more pages, then we have to repeat this same method for all of them.
-      BUT, it's not the optimized way of doing Routing, instead we will use "react-router"
+      {/* Routes is the parent, and
+      Route(s) are children
       */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 };
