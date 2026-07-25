a876bf85e5f9a61d8aff71c19765e40e3c981c50
Aryan Saini <09aryan.lucky@gmail.com>
2026-07-25 15:27:45 +0530

    day14: routing without react-router

--- React/day14-react_router/src/App.jsx
+++ React/day14-react_router/src/App.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 
 const App = () => {
+  const [toggle, setToggle] = useState("home");
   return (
     <div className="h-screen p-2">
       <nav className="flex justify-between border p-2">
         <h1>Logo</h1>
-        <ul className="flex gap-4">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+        <ul className="flex gap-4 cursor-pointer">
+          <li onClick={() => setToggle("home")}>Home</li>
+          <li onClick={() => setToggle("about")}>About</li>
+          <li onClick={() => setToggle("contact")}>Contact</li>
           <li>Services</li>
           <li>Enterprise</li>
           <li>Shopping</li>
@@ -19,9 +20,12 @@ const App = () => {
         <button>Login</button>
       </nav>
 
-      <Home />
-      <About />
-      <Contact />
+      {toggle === "home" && <Home />}
+      {toggle === "about" && <About />}
+      {toggle === "contact" && <Contact />}
+      {/* if we have 10 or 15 or more pages, then we have to repeat this same method for all of them.
+      BUT, it's not the optimized way of doing Routing, instead we will use "react-router"
+      */}
     </div>
   );
 };
