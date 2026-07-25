ce43bb09eccf4a5aaf7254b7a3dfe4c3f93b7454
Aryan Saini <09aryan.lucky@gmail.com>
2026-07-25 22:16:47 +0530

    day14: arranged code in structured way

--- React/day14-react_router/src/App.jsx
+++ React/day14-react_router/src/App.jsx
@@ -1,30 +1,14 @@
 import React, { useState } from "react";
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
 import { Route, Routes, NavLink } from "react-router";
+import Navbar from "./components/Navbar";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
     <div className="h-screen p-2">
-      <nav className="flex justify-between border p-2">
-        <h1>Logo</h1>
-        <ul className="flex gap-4 cursor-pointer">
-          <NavLink to={"/"}>Home</NavLink>
-          <NavLink to={"/about"}>About</NavLink>
-          <NavLink to={"/contact"}>Contact</NavLink>
-        </ul>
-        <button>Login</button>
-      </nav>
+      <Navbar />
 
-      {/* Routes is the parent, and
-      Route(s) are children
-      */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <AppRoutes />
     </div>
   );
 };
