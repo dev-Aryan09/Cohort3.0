527027cf67d289ed21187bb6b319df023fa94cdd
Aryan Saini <09aryan.lucky@gmail.com>
2026-07-28 23:11:28 +0530

    create routes

--- Assignments/skyMart/src/App.jsx
+++ Assignments/skyMart/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
+      <AppRoutes />
     </div>
   );
 };
--- Assignments/skyMart/src/routes/AppRoutes.jsx
+++ Assignments/skyMart/src/routes/AppRoutes.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import About from "../pages/About";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRoutes = () => {
-  return <div>AppRoutes</div>;
+  return (
+    <div className="flex flex-col gap-4 pt-16">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default AppRoutes;
