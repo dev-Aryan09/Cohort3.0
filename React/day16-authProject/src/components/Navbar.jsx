baa30f753bdf90a6977e71f9b6e7453e88c8e8d2
Aryan Saini <09aryan.lucky@gmail.com>
2026-08-03 23:52:27 +0530

    day16: update navbar

--- React/day16-authProject/src/components/Navbar.jsx
+++ React/day16-authProject/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -8,15 +8,44 @@ const Navbar = () => {
     navigate("/");
   };
   return (
-    <div className="flex justify-between py-4 px-12 rounded text-xl bg-slate-600 text-white">
+    <div className="flex flex-col justify-between rounded text-xl border-r-2 px-1">
       <div className="cursor-pointer">logo</div>
-      <ul className="flex gap-10">
-        <li className="cursor-pointer">Home</li>
-        <li className="cursor-pointer">Cart</li>
+      <ul className="flex flex-col gap-4 text-center text-lg">
+        <NavLink
+          to="/main"
+          className={({ isActive }) => {
+            return isActive
+              ? "text-red-500 font-semibold italic cursor-pointer"
+              : "cursor-pointer";
+          }}
+          end
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/main/users"
+          className={({ isActive }) => {
+            return isActive
+              ? "text-red-500 font-semibold italic cursor-pointer"
+              : "cursor-pointer";
+          }}
+        >
+          Users
+        </NavLink>
+        <NavLink
+          to="/main/products"
+          className={({ isActive }) => {
+            return isActive
+              ? "text-red-500 font-semibold italic cursor-pointer"
+              : "cursor-pointer";
+          }}
+        >
+          Products
+        </NavLink>
       </ul>
       <button
         onClick={handleLogout}
-        className="border p-1 bg-red-500 cursor-pointer"
+        className="border p-1 bg-red-500 rounded text-white cursor-pointer"
       >
         Logout
       </button>
