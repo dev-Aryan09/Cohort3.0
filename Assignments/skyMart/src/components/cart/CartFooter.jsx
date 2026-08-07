16895fb694db2e4e33c07baab0f9585351bf58a1
Aryan Saini <09aryan.lucky@gmail.com>
2026-08-07 22:44:30 +0530

    create checkout and clear cart logic

--- Assignments/skyMart/src/components/cart/CartFooter.jsx
+++ Assignments/skyMart/src/components/cart/CartFooter.jsx
@@ -1,8 +1,22 @@
 import React, { useContext } from "react";
 import { MyStore } from "../../context/MyContext";
+import { toast } from "react-toastify";
 
 const CartFooter = () => {
-  const { totalCartPrice } = useContext(MyStore);
+  const { totalCartPrice, setCartItems, setCartOpen } = useContext(MyStore);
+
+  const handleCheckOut = () => {
+    localStorage.removeItem("cartItems");
+    toast.success("Order place!🎉 (Demo)", {
+      position: "bottom-right",
+      autoClose: 2000,
+      pauseOnHover: false,
+      draggable: true,
+      theme: "dark",
+    });
+    setCartItems([]);
+    setCartOpen(false);
+  };
 
   return (
     <div className="border-t border-white py-3 px-4">
@@ -12,11 +26,18 @@ const CartFooter = () => {
         <span className="text-xl font-semibold text-white underline underline-offset-3">{`$${totalCartPrice / 100}`}</span>
       </div>
 
-      <button className="w-full rounded-2xl bg-lime-400 py-2 text-xl font-semibold text-black cursor-pointer">
+      <button
+        onClick={handleCheckOut}
+        className="w-full rounded-2xl bg-lime-400 py-2 text-xl font-semibold text-black cursor-pointer"
+      >
         Checkout →
       </button>
 
       <button
+        onClick={() => {
+          localStorage.removeItem("cartItems");
+          setCartItems([]);
+        }}
         title="delete all products"
         className="mt-3 w-full text-neutral-500 text-xs hover:text-red-500 cursor-pointer"
       >
